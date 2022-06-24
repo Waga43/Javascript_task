bf7929d13c5a133e7d313d6200e4c6d34c278261
EduTechTainMent <44534503+Waga43@users.noreply.github.com>
2022-06-24 12:51:01 +0100

    Added both html and javascriptfiles
    First commit

---
+++ screen.js
@@ -0,0 +1,12 @@
+// First using the alert method to print to the screen
+
+alert("My name: Abdulwasiu B. Popoola")
+alert("My height: 1.8m")
+alert("My country: Nigeria")
+
+// Second, using the document.write method to print to the screen
+document.write("<strong>My name:</strong> Abdulwasiu B. Popoola")
+document.write("<br>")
+document.write("<strong>My height:</strong> 1.8m")
+document.write("<br>")
+document.write("<strong>My country:</strong> Nigeria")
